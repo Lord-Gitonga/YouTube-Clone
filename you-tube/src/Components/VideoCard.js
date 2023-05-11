19e0a75c5be3b0b654f1e43b5b384abfff9e18fd
lordish <gitonga403@gmail.com>
2023-05-11 09:23:29 +0300

    Add viewing feature to individual channel

--- you-tube/src/Components/VideoCard.js
+++ you-tube/src/Components/VideoCard.js
@@ -6,7 +6,9 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 function VideoCard({ video: {id: { videoId }, snippet} }) {
   return (
-      <Card>
+      <Card sx={{
+          width: { md: '320px', xs: '100%' }, boxShadow: 'none',
+      borderRadius: '10px'}}>
           <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
               <CardMedia
                   image={snippet?.thumbnails?.high?.url}
@@ -14,6 +16,19 @@ function VideoCard({ video: {id: { videoId }, snippet} }) {
                   sx={{width: 358, height: 180}}
               />
           </Link>
+          <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
+              <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                  <Typography variant='subtitle1' fontWeight='bold' color='#FFF'>
+                      {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                  </Typography>
+              </Link>
+              <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                  <Typography variant='subtitle2' fontWeight='bold' color='gray'>
+                      {snippet?.channelTitle || demoChannelTitle}
+                      <CheckCircle sx={{fontSize: 12, color: 'grey', ml: '5px'}} />
+                  </Typography>
+              </Link>
+          </CardContent>
    </Card>
   )
 }
